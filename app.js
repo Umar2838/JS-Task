d63f2e58b022e5f12a7372ba64830e08c6e08cfd
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-02-19 17:54:37 +0500

    addedc a task

---
+++ app.js
@@ -0,0 +1,48 @@
+
+//<********************************Sorting *********************> 
+
+
+
+//<*************************Smaller Number*************************>
+
+// var number = [24, 53, 78, 91, 12];
+// var smallest = number[0];
+
+// for (var i = 0; i < number.length; i++) {
+//   if (number[i] < smallest) {
+//     smallest = number[i];
+//   }
+// }
+// alert("Alert Items: "+number)
+// alert("The smallest number in the array is: " + smallest);
+
+
+//<********************Greater Number*************************>
+
+// var number = [24, 53, 78, 91, 12];
+// var largest = number[0];
+
+// for (var i = 0; i < number.length; i++) {
+//   if (number[i] > largest) {
+//     largest = number[i];
+//   }
+// }
+// alert("Alert Items: "+number)
+// alert("The largest number in the array is: " + largest);
+
+//<**********************Missing Number*************************>
+
+// var numbers = [25,26,27,28,29,31,32,33,34,35];
+// var missingNumber = numbers[0] - 1;
+// for (let i = 0; i < numbers.length; i++) 
+//   if (numbers[i] - missingNumber > 1) {
+//     missingNumber += 1;
+//     break;
+//   }
+//    else {
+//     missingNumber = numbers[i];
+//   }
+
+
+// console.log("Missing number is:", missingNumber);
+
